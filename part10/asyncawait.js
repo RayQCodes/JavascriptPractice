4c122c21198b332d027d79a82f92af1717fc95d0
RayQCodes <rquan3@gmu.edu>
2025-06-09 18:04:15 -0400

    more practice

---
+++ part10/asyncawait.js
@@ -0,0 +1,18 @@
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ name: "chaicode", url: "https://chaicode.com" });
+    }, 3000);
+  });
+}
+
+async function getuserdata() {
+  try {
+    console.log("fetching user data...");
+    const userData = await fetchData();
+    console.log("User Data: ", userData);
+  } catch (error) {
+    console.log("user data fetch unsuccesful", error);
+  }
+}
+getuserdata();
