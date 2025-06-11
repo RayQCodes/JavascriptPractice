37cde20494386ab4790716a8061e930795948101
RayQCodes <rquan3@gmu.edu>
2025-06-11 15:24:44 -0400

    awaiting promises

--- part10/asyncawait.js
+++ part10/asyncawait.js
@@ -1,19 +1,54 @@
-//this fetch data
-function fetchData() {
-  return new Promise((resolve, reject) => {
+// //this fetch data
+// function fetchData() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       reject({ name: "chaicode", url: "https://chaicode.com" });
+//     }, 3000);
+//   });
+// }
+
+// async function getuserdata() {
+//   try {
+//     console.log("fetching user data...");
+//     const userData = await fetchData();
+//     console.log("User Data: ", userData);
+//   } catch (error) {
+//     console.log("user data fetch unsuccesful", error);
+//   }
+// }
+// getuserdata();
+
+function fetchPostData() {
+  return new Promise((resolve) => {
     setTimeout(() => {
-      resolve({ name: "chaicode", url: "https://chaicode.com" });
+      resolve("Post Data Fetched");
+    }, 2000);
+  });
+}
+
+function fetchCommentData() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("comment data fetched");
     }, 3000);
   });
 }
 
-async function getuserdata() {
+async function getBlogData() {
   try {
-    console.log("fetching user data...");
-    const userData = await fetchData();
-    console.log("User Data: ", userData);
+    // const blogData = await fetchPostData();
+    // const commentdata = await fetchCommentData();
+    let [blogData, commentData] = await Promise.all([
+      fetchPostData(),
+      fetchCommentData(),
+    ]);
+    console.log(blogData);
+    console.log(commentData);
+
+    console.log("fetch complete");
   } catch (error) {
-    console.log("user data fetch unsuccesful", error);
+    console.log("Error fetching blog data");
   }
 }
-getuserdata();
+
+getBlogData();
