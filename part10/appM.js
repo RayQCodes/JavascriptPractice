df9f41f2e7e45c0f694d447e82a04b9d5f2c3079
RayQCodes <rquan3@gmu.edu>
2025-06-12 14:57:27 -0400

    module js

---
+++ part10/appM.js
@@ -0,0 +1,12 @@
+// default imports
+import multiply from "./MathoperationsM.js";
+
+console.log(multiply(1, 2));
+
+//named import
+
+import { add, subtract } from "./MathoperationsM.js";
+
+console.log(add(2, 3));
+
+console.log(subtract(2, 4));
